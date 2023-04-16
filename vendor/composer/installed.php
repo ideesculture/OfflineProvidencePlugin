94553d2f1a1190301e564d498912bd879cb4d412
Gautier MICHELIN <gm@ideesculture.com>
2023-04-16 18:23:45 +0200

    vendor for Providence plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'swaggest/json-diff' => array(
+            'pretty_version' => 'v3.10.4',
+            'version' => '3.10.4.0',
+            'reference' => 'f4e511708060ff7511a3743fab4aa484a062bcfb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../swaggest/json-diff',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
