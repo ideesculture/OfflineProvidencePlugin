0fd23c77a042c3d1217366f44fd1370d3ad76c00
Gautier MICHELIN <gm@ideesculture.com>
2023-04-16 18:23:32 +0200

    controller & curl lib for providence plugin

--- controllers/StoreController.php
+++ controllers/StoreController.php
@@ -1,1 +1,84 @@
 <?php
+
+use Swaggest\JsonDiff\JsonDiff;
+
+require_once __CA_MODELS_DIR__ . "/ca_objects.php";
+require_once __CA_APP_DIR__ . "/plugins/OfflineProvidence/lib/curl_ca_ws.php";
+error_reporting(E_ERROR);
+ini_set("display_errors", "on");
+
+
+class StoreController extends ActionController
+{
+	private $opo_config;
+	private $ps_plugin_path;
+	private $authToken;
+	# -------------------------------------------------------
+	#
+	# -------------------------------------------------------
+	public function __construct(&$po_request, &$po_response, $pa_view_paths = null)
+	{
+		parent::__construct($po_request, $po_response, $pa_view_paths);
+
+		// Global vars for all children views
+		$this->view->setVar('plugin_dir', __CA_BASE_DIR__ . "/app/plugins/OfflineProvidence");
+		$this->view->setVar('plugin_url', __CA_URL_ROOT__ . "/app/plugins/OfflineProvidence");
+
+		$this->ps_plugin_path = __CA_BASE_DIR__ . "/app/plugins/OfflineProvidence";
+
+		if (file_exists($this->ps_plugin_path . '/conf/local/OfflineProvidence.conf')) {
+			$this->opo_config = Configuration::load($this->ps_plugin_path . '/conf/local/OfflineProvidence.conf');
+		} else {
+			$this->opo_config = Configuration::load($this->ps_plugin_path . '/conf/OfflineProvidence.conf');
+		}
+		$this->authToken = getAuthToken();
+	}
+
+	public function Populate()
+	{
+		$campagne_id = $this->getRequest()->getParameter("campagne_id", pInteger);
+		$o_data = new Db();
+		$query = "
+		SELECT coo.occurrence_left_id as id 
+		FROM ca_occurrences co 
+		LEFT JOIN ca_occurrences_x_occurrences coo on co.occurrence_id = coo.occurrence_left_id 
+		WHERE deleted =0 
+		AND relation_id is not null 
+		AND occurrence_right_id =$campagne_id
+ 		";
+
+		$qr_res = $o_data->query($query);
+		
+		$fileList = [];
+		while ($qr_res->nextRow()) {
+			$infos = getOccurrenceDetails($qr_res->get("id"), $this->authToken);
+			file_put_contents($this->ps_plugin_path . "/json/67_" . $qr_res->get("id") . ".json", $infos);
+			$fileList[] = $this->ps_plugin_path . "/json/67_" . $qr_res->get("id") . ".json";
+		}
+
+		$this->view->setVar("fileList", $fileList);
+		$this->render("store_populate_html.php");
+	}
+
+	public function Import() {
+		require __DIR__ . '/../vendor/autoload.php';
+		error_reporting(E_ERROR);
+		ini_set("display_errors", true);
+		$vn_tablenum = $this->request->getParameter("tablenum", pInteger);
+		$vn_id = $this->request->getParameter("id", pInteger);
+
+		$vs_json = $this->getRequest()->getParameter("json", pString, "POST");
+		$vt_json = json_decode($vs_json, true);
+		// ICI FAIRE LA COMPARAISON
+		$vs_actual_ca_json = getOccurrenceDetails($vn_id, $this->authToken);
+		$vt_ca_json = json_decode($vs_actual_ca_json, true);
+		
+		$r = new JsonDiff(json_decode($vs_json), json_decode($vs_actual_ca_json));
+		var_dump($vs_json);
+		var_dump($vs_actual_ca_json);
+		var_dump($r);
+		// ICI AFFICHER LE DELTA
+		die();
+	}
+}
+
---
+++ lib/curl_ca_ws.php
@@ -0,0 +1,138 @@
+<?php
+
+function getAuthToken() {
+	$curl = curl_init(__CA_SITE_PROTOCOL__."://".__DH_CA_WS_USER__.":".__DH_CA_WS_PASS__."@".__CA_SITE_HOSTNAME__.__CA_URL_ROOT__."/service.php/auth/login");
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_error($curl)) {
+		die("probleme connexion curl");
+	} else {
+		$content = curl_exec($curl);
+
+		// Check the return value of curl_exec(), too
+		if ($content === false) {
+			throw new Exception(curl_error($curl), curl_errno($curl));
+		}
+		$result = json_decode($content, true);
+		$result = $result["authToken"];
+		curl_close ($curl);
+	}
+
+	return $result;
+}
+
+function getObjDetail($id, $authToken) {
+	$url = __CA_SITE_PROTOCOL__."://".__DH_CA_WS_USER__.":".__DH_CA_WS_PASS__."@".__CA_SITE_HOSTNAME__.__CA_URL_ROOT__."/service.php/item/ca_objects/id/".$id."?pretty=1&authToken=".$authToken;
+	//var_dump($url);
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_error($curl)) {
+		$result = false;
+		die("probleme connexion curl");
+	} else {
+		$result = json_decode(curl_exec($curl));
+		//print $result ;
+		//$result = json_decode(curl_exec ($curl), true);
+		curl_close ($curl);
+	}
+	return $result;
+}
+
+function getOccurrenceDetails($id, $authToken) {
+	$url = __CA_SITE_PROTOCOL__."://".__DH_CA_WS_USER__.":".__DH_CA_WS_PASS__."@".__CA_SITE_HOSTNAME__.__CA_URL_ROOT__."/service.php/item/ca_occurrences/id/".$id."?pretty=1&authToken=".$authToken;
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_error($curl)) {
+		$result = false;
+		die("probleme connexion curl");
+	} else {
+		//$result = json_decode(curl_exec($curl));
+		//print $result ;
+		$result = curl_exec ($curl);
+		curl_close ($curl);
+	}
+	return $result;
+}
+
+function writeOccurrenceDetails($id, $json, $authToken) {
+	$url = __CA_SITE_PROTOCOL__."://".__DH_CA_WS_USER__.":".__DH_CA_WS_PASS__."@".__CA_SITE_HOSTNAME__.__CA_URL_ROOT__."/service.php/item/ca_occurrences/id/".$id."?pretty=1&authToken=".$authToken;
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if($msg = curl_error($curl)) {
+		$result = false;
+		var_dump($msg);
+		die("probleme connexion curl");
+	} else {
+		//$result = json_decode(curl_exec($curl));
+		//print $result ;
+		$result = curl_exec ($curl);
+		curl_close ($curl);
+	}
+	return $result;
+}
+
+
+function getModelObject($type,$authToken){
+	$url = __CA_SITE_PROTOCOL__."://".__DH_CA_WS_USER__.":".__DH_CA_WS_PASS__."@".__CA_SITE_HOSTNAME__.__CA_URL_ROOT__."/service.php/model/ca_objects?pretty=1&format=edit&authToken=".$authToken;
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_error($curl)) {
+		$result = false;
+		die("probleme connexion curl");
+	} else {
+		$result = json_decode(curl_exec($curl));
+		//print $result ;
+		//$result = json_decode(curl_exec ($curl), true);
+		curl_close ($curl);
+	}
+	return $result->{$type};
+}
+
+
+/**
+ * 
+ * @param mixed $model : Model du type d'objet
+ * @param mixed $metadata 
+ * @param mixed $submetadata 
+ * @return mixed 
+ */
+function getMetadataLabelFromModel($model,$metadata, $submetadata = null){
+	if (!$submetadata){
+		$label = $model->elements->{$metadata}->name;
+	}
+	if ($submetadata){
+		$label = $model->elements->{$metadata}->elements_in_set->$submetadata->display_label;
+	}
+	
+	return $label;
+}
+
+/**
+ * 
+ * Function that returns the object type idno 
+ * 
+ * @param int $type_id ID Of the Object Type
+ * @param string $authToken Authentification token for the webservice
+ * @return string  
+ */
+function getObjectTypeCode($type_id, $authToken){
+	$url = __CA_SITE_PROTOCOL__."://".__DH_CA_WS_USER__.":".__DH_CA_WS_PASS__."@".__CA_SITE_HOSTNAME__.__CA_URL_ROOT__."/service.php/item/ca_list_items/id/".$type_id."?pretty=1&authToken=".$authToken;
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_error($curl)) {
+		$result = false;
+		die("probleme connexion curl");
+	} else {
+		$result = json_decode(curl_exec($curl));
+		//print $result ;
+		//$result = json_decode(curl_exec ($curl), true);
+		curl_close ($curl);
+	}
+	
+	return $result->intrinsic->idno;
+}
